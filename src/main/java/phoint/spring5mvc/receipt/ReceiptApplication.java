c553e502640ffea8d69d38e2d27165caffb1de36
PhoiNT2806 <phoi.nt2806@gmail.com>
2022-12-09 00:16:23 +0700

    Init Receipt Project

---
+++ src/main/java/phoint/spring5mvc/receipt/ReceiptApplication.java
@@ -0,0 +1,13 @@
+package phoint.spring5mvc.receipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phoint/spring5mvc/receipt/ReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package phoint.spring5mvc.receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
