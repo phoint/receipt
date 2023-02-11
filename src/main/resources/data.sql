790d557734d3a7d08ab08d2a76e9724b23764c47
PhoiNT2806 <phoi.nt2806@gmail.com>
2023-02-11 13:11:19 +0700

    Add sql script for data initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (descriptions) VALUES ('American');
+INSERT INTO category (descriptions) VALUES ('Italian');
+INSERT INTO category (descriptions) VALUES ('Mexixan');
+INSERT INTO category (descriptions) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (descriptions) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (descriptions) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (descriptions) VALUES ('Cup');
+INSERT INTO unit_of_measure (descriptions) VALUES ('Pinch');
+INSERT INTO unit_of_measure (descriptions) VALUES ('Ounce');
